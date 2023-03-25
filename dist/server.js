78182249ac8d06917659e34989ca8ca7f5c47f95
Raul Rojas <38226590+Wohsaka@users.noreply.github.com>
2023-03-25 11:03:15 -0400

    Delete dist directory

--- dist/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.listen(4000, () => console.log('[SERVER]: Server running on port 4000...'));
